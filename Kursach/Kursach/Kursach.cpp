3a56493b5f027f77bab52b16bbba6699fcb414f2
Dikobrazzzec <zasxcd1307@mail.ru>
2019-11-19 07:01:17 +0300

    19.11.19 7:01

--- Kursach/Kursach/Kursach.cpp
+++ Kursach/Kursach/Kursach.cpp
@@ -3,19 +3,19 @@
 #include <iostream>
 #include <math.h>
 #define M_PI
-																									
+
 using namespace std;
 
 class Angles {
 public:
 	float Deg, Min;
 private:
-	const float pi =float (M_PI) ;
+	const float pi = float(M_PI);
 	float Rad;
-	Angles * pointer1;
-	Angles * pointer2;
+	Angles* pointer1;
+	Angles* pointer2;
 public:
-	Angles() {} 
+	Angles() {}
 
 	Angles(float Deg_, float Min_) {
 		Deg = Deg_;
@@ -27,7 +27,7 @@ public:
 		float MinNewAdd = Min + ParAdd.Min;
 		ParAdd.Deg = DegNewAdd;
 		ParAdd.Min = MinNewAdd;
-		cout << "Degrees = " << DegNewAdd << "° Minutes = " << MinNewAdd << "'";
+		cout << "Degrees = " << DegNewAdd << "° Minutes = " << MinNewAdd << "'" << endl;
 		return ParAdd;
 	}
 
@@ -36,40 +36,65 @@ public:
 		float MinNewSubtr = Min - ParSubtr.Min;
 		ParSubtr.Deg = DegNewSubtr;
 		ParSubtr.Min = MinNewSubtr;
-		cout << "Degrees = " << DegNewSubtr << "° Minutes = " << MinNewSubtr << "'";
+		cout << "Degrees = " << DegNewSubtr << "° Minutes = " << MinNewSubtr << "'" << endl;
 		return ParSubtr;
 	}
-																								//Сделать конструктор для градусов и минут
-																								//Подумать над отрицательными углами;															
-																								//Conversion - преобразование, cast - приведение
-	Angles operator<(Angles ParConv) {								
-		while (ParConv.Deg >= 360)
-		{
+	//Сделать конструктор для градусов и минут
+	//Подумать над отрицательными углами;															
+	//Conversion - преобразование, cast - приведение
+	Angles operator<(Angles ParConv) {
+		while (ParConv.Deg >= 360) {
 			ParConv.Deg -= 360;
 		}
-		while (ParConv.Min >= 60 )
-		{
+		while (ParConv.Min >= 60) {
 			ParConv.Min -= 60;
 		}
-		float DegNewConv = ParConv.Deg;
+		float DegNewConv = ParConv.Deg; 
 		float MinNewConv = ParConv.Min;
-		cout << "Degrees = " << DegNewConv << "° Minutes = " << MinNewConv << "'";
+		cout << "Degrees = " << DegNewConv << "° Minutes = " << MinNewConv << "'" << endl;
 		return ParConv;
 	}
 
-	Angles operator*(Angles ParCast) {															//rad  = deg*(pi/180)  rad = (min/60)*(pi/180)
-		ParCast.operator<(ParCast);
-		float RadNew = (ParCast.Deg * (pi / 180)) + ((ParCast.Min / 60) * (pi / 180));
-		ParCast.Rad = RadNew;
-		cout << "Degrees = " << ParCast.Deg << "° Minutes = " << ParCast.Min << "' Radians = " << ParCast.Rad;
+	Angles operator=(Angles ParPrisv) {
+		Angles Ret;
+		Ret.Deg = ParPrisv.Deg;
+		Ret.Min = ParPrisv.Min;
+		cout << Ret.Deg << " " << Ret.Min << " = " << endl;
+		pointer1 = &Ret;
+		pointer1->CreatRad();
+		return Ret;
+	} 
+
+	Angles operator*(Angles ParCast) {
+		ParCast = ParCast.operator<(ParCast);
+		//pointer1->CreatRad();
 		return ParCast;
 	}
+
+	Angles CreatRad() {
+		//float RadNew = (Deg * (pi / 180)) + ((Min / 60) * (pi / 180));
+		cout << Deg << " " << Min << pi<< " 1111" << endl;													//Сделать нормальное значение для pi. Остальное работает.
+		float RadNew = (Deg * (pi / 180)) + ((Min / 60) * (pi / 180));
+		cout << "Degrees = " << Deg << "° Minutes = " << Min << "' Radians = " << RadNew << endl;
+		Angles Ret;
+		Ret.Deg = Deg;
+		Ret.Min = Min;
+		Ret.Rad = RadNew;
+		return Ret;
+	}
+																																						//rad  = deg*(pi/180)  rad = (min/60)*(pi/180)
+/*
+	void print() {
+		cout << Deg << " " << Min << " print" << endl;
+	} */
 };
 
 void main() {
-	Angles J(4454,5655);
-	Angles K(5,7);
-	J.operator<(J);
+	Angles J(4454, 5655);
+	Angles K(5, 7);
+	//  J.operator-(K);
+	//  J.operator+(K);
+	//	J.operator<(J);
 	Angles Q;
-	J.operator+(K);
+	J.operator*(J);
 }
